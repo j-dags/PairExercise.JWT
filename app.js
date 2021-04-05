022cd1667fffef4a51e92a2b530dc92500a5694b
j-dags <jon.dagdagan@gmail.com>
2021-04-05 10:11:10 -0500

    section 2:jwt

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-const { models: { User }} = require('./db');
-const path = require('path');
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+app.use(express.json())
+const {
+	models: { User },
+} = require('./db')
+const path = require('path')
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.use(morgan('tiny'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
-app.post('/api/auth', async(req, res, next)=> {
-  try {
-    res.send({ token: await User.authenticate(req.body)});
-  }
-  catch(ex){
-    next(ex);
-  }
-});
+const requireToken = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization
+		const user = await User.byToken(token)
+		req.user = user
+		next()
+	} catch (error) {
+		next(error)
+	}
+}
 
-app.get('/api/auth', async(req, res, next)=> {
-  try {
-    res.send(await User.byToken(req.headers.authorization));
-  }
-  catch(ex){
-    next(ex);
-  }
-});
+app.post('/api/auth', async (req, res, next) => {
+	try {
+		const user = await User.authenticate(req.body)
+		if (!user) res.sendStatus(404)
+		const token = await jwt.sign({ id: user }, process.env.JWT)
+		res.send({ token })
+	} catch (ex) {
+		next(ex)
+	}
+})
 
-app.use((err, req, res, next)=> {
-  console.log(err);
-  res.status(err.status || 500).send({ error: err.message });
-});
+app.get('/api/auth', async (req, res, next) => {
+	try {
+		// res.send(await User.byToken(req.headers.authorization))
+		const token = req.headers.authorization
+		const user = await User.byToken(token)
+		req.user = user
+		res.send(req.user)
+	} catch (ex) {
+		next(ex)
+	}
+})
 
-module.exports = app;
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.status(err.status || 500).send({ error: err.message })
+})
+
+module.exports = app
--- db.js
+++ db.js
@@ -1,73 +1,79 @@
-const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+const jwt = require('jsonwebtoken')
+const Sequelize = require('sequelize')
+const { STRING } = Sequelize
 const config = {
-  logging: false
-};
-
-if(process.env.LOGGING){
-  delete config.logging;
+	logging: false,
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+
+if (process.env.LOGGING) {
+	delete config.logging
+}
+const conn = new Sequelize(
+	process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+	config
+)
 
 const User = conn.define('user', {
-  username: STRING,
-  password: STRING
-});
+	username: STRING,
+	password: STRING,
+})
 
-User.byToken = async(token)=> {
-  try {
-    const user = await User.findByPk(token);
-    if(user){
-      return user;
-    }
-    const error = Error('bad credentials');
-    error.status = 401;;
-    throw error;
-  }
-  catch(ex){
-    const error = Error('bad credentials');
-    error.status = 401;;
-    throw error;
-  }
-};
+User.byToken = async (token) => {
+	try {
+		console.log('INCOMING TOKEN ~~~>', token)
+		const payload = await jwt.verify(token, process.env.JWT)
+		const user = await User.findByPk(payload.id)
+		if (user) {
+			console.log('Deserialized Payload ~~~~>', payload)
+			return user
+		}
+		const error = Error('byToken: BAD CREDENTIAL')
+		error.status = 401
+		throw error
+	} catch (ex) {
+		const error = Error('byToken: BAD CREDENTIAL')
+		error.status = 401
+		throw error
+	}
+}
 
-User.authenticate = async({ username, password })=> {
-  const user = await User.findOne({
-    where: {
-      username,
-      password
-    }
-  });
-  if(user){
-    return user.id; 
-  }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
-};
+User.authenticate = async ({ username, password }) => {
+	const user = await User.findOne({
+		where: {
+			username,
+			// password,
+		},
+	})
+	if (user) {
+		return user.id
+	}
+	const error = Error('bad credentials')
+	error.status = 401
+	throw error
+}
 
-const syncAndSeed = async()=> {
-  await conn.sync({ force: true });
-  const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
-  ];
-  const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
-  );
-  return {
-    users: {
-      lucy,
-      moe,
-      larry
-    }
-  };
-};
+const syncAndSeed = async () => {
+	await conn.sync({ force: true })
+	const credentials = [
+		{ username: 'lucy', password: 'lucy_pw' },
+		{ username: 'moe', password: 'moe_pw' },
+		{ username: 'larry', password: 'larry_pw' },
+	]
+	const [lucy, moe, larry] = await Promise.all(
+		credentials.map((credential) => User.create(credential))
+	)
+	return {
+		users: {
+			lucy,
+			moe,
+			larry,
+		},
+	}
+}
 
 module.exports = {
-  syncAndSeed,
-  models: {
-    User
-  }
-};
+	syncAndSeed,
+	models: {
+		User,
+	},
+}
